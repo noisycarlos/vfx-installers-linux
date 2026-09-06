c49841a75b4588557de150b2ef68603aa0d28959
Carlos Aldana <noisycarlos@hey.com>
2026-09-06 08:48:39 -0700

    fixes resolve and adds fuse install

--- resolve.sh
+++ resolve.sh
@@ -32,13 +32,15 @@ fi
 version=$(echo ${installer_path} | sed -n 's/.\/DaVinci_Resolve_Studio_\([0-9.]*\)_Linux.run/\1/p')
 echo "--- Installing ${app_name} version ${version}..."
 
-# sudo SKIP_PACKAGE_CHECK=1 ${installer_path} -i -y
-
 if command -v dnf &>/dev/null; then
   sudo dnf install apr apr-util libxcb xcb-util-cursor xcb-util-damage
+  sudo dnf install fuse fuse-libs -y
 elif command -v apt &>/dev/null; then
   sudo apt install libapr1 libaprutil1 libxcb-cursor0 libxcb-damage0
+  sudo apt install libfuse2 -y
 fi
+
+sudo SKIP_PACKAGE_CHECK=1 ${installer_path} -i -y
 
 echo "--- Moving uneeded libraries..."
 
